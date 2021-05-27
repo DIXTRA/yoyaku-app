53bfeb832bc7168990b34f8462da26e5c053c309
Mathias Morales <mathias.morales@dixtra.co>
2021-05-27 13:28:01 -0300

    remove bolt and add node-slack-sdk

--- app.js
+++ app.js
@@ -1,82 +1,40 @@
-/* eslint-disable no-console */
-const { App, ExpressReceiver } = require('@slack/bolt');
-const mongoose = require('mongoose');
+const express = require('express');
 const AdminBro = require('admin-bro');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const createError = require('http-errors');
-const path = require('path');
-const options = require('./admin/admin.options');
+const mongoose = require('mongoose');
+const { createEventAdapter } = require('@slack/events-api');
+const { createMessageAdapter } = require('@slack/interactive-messages');
+const { WebClient } = require('@slack/web-api');
 const buildAdminRouter = require('./admin/admin.router');
+const options = require('./admin/admin.options');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const adminBro = new AdminBro(options);
 const router = buildAdminRouter(adminBro);
+const app = express();
+const token = process.env.SLACK_BOT_TOKEN;
+const webClient = new WebClient(token);
 
-// Create a Bolt Receiver
-const receiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET);
+
+app.use(adminBro.options.rootPath, router);
+app.use('/slack/events', slackEvents.expressMiddleware());
+app.use('/slack/actions', slackInteractions.expressMiddleware());
+
+app.use(express.json());
+
+slackEvents.on('message', async (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
 
-// Create the Bolt App, using the receiver
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  receiver,
+// Starts server
+app.listen(port, async () => {
+  await mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`Server is listening on port ${port}`);
 });
-
-// Slack interactions are methods on app
-
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
-});
-
-// Other web requests are methods on receiver.router
-receiver.router.use(adminBro.options.rootPath, router);
-
-// // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
-
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
-(async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    await app.start(PORT);
-    console.log('⚡️ Bolt app is running!');
-  } catch (e) {
-    console.log(e);
-  }
-})();
 
 module.exports = app;
