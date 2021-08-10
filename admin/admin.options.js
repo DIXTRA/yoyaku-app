0bde51b84c850e7c2369d15ab7ff23a4fe303113
Mauricio Guzman <mauricio.guzman@dixtra.co>
2021-08-10 20:59:39 -0300

    Fix: when loading User resource it fails because it cant associate with Team

--- admin/admin.options.js
+++ admin/admin.options.js
@@ -6,11 +6,12 @@ AdminBro.registerAdapter(AdminBroMongoose);
 const AdminUser = require('../entities/user.admin');
 const AdminOffice = require('../entities/office.admin');
 const AdminRoom = require('../entities/room.admin');
+const AdminTeam = require('../entities/team.admin');
 
 /** @type {AdminBro.AdminBroOptions} */
 
 const options = {
-  resources: [AdminUser, AdminOffice, AdminRoom],
+  resources: [AdminUser, AdminOffice, AdminRoom, AdminTeam],
   branding: {
     companyName: 'Yoyaku Admin',
   },
